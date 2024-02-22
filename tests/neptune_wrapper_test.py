27af1facc817b26360c564cdac2ae4d04eb70a6e
bryzgalovdm <bryzgalovdm@gmail.com>
2024-02-22 21:29:46 +0100

    attempt starting ci by making test imports relative

--- tests/neptune_wrapper_test.py
+++ tests/neptune_wrapper_test.py
@@ -2,7 +2,7 @@
 
 import pytest
 from unittest.mock import MagicMock, patch
-from neptune_wrapper.neptune_wrapper import NeptuneWrapper
+from ..neptune_wrapper.neptune_wrapper import NeptuneWrapper
 
 @pytest.fixture
 def neptune_client_mock():
