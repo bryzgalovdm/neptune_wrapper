7bae28b84279c438521c1238395fb8d30cf1c636
bryzgalovdm <bryzgalovdm@gmail.com>
2024-02-27 22:35:39 +0100

    add neptune_wrapper unit tests
    basic functionality - run and callback creation - are tested.
    neptune_wrapper is changed to correct errors caught by testing.
    tensorflow callback is added to the requirements

--- neptune_wrapper/neptune_wrapper.py
+++ neptune_wrapper/neptune_wrapper.py
@@ -12,11 +12,10 @@ from .utils import Params
 class NeptuneWrapper():
 
     def __init__(self, projectName=None, api='', dataDir = '', params = None,
-                 metrics = None, results = None, accuracySub = None,
-                 figDict = None, sourceFiles = ['environment.yml',
-                                                'requirements.txt',
-                                                'model.py',
-                                                'run.py'], **kwargs):
+                 sourceFiles = ['environment.yml',
+                                'requirements.txt',
+                                'model.py',
+                                'run.py'], **kwargs):
         self.projectName = projectName
         self.api = api
         if os.name == 'posix':
@@ -24,10 +23,6 @@ class NeptuneWrapper():
         elif os.name == 'nt':
             self.dataDir = f'file://{dataDir}'
         self.params = params
-        self.metrics = metrics
-        self.results = results
-        self.accuracySub = accuracySub
-        self.figDict = figDict
         self.sourceFiles = sourceFiles
 
         for key, value in kwargs.items():
@@ -42,17 +37,19 @@ class NeptuneWrapper():
         paramsDict = {a: getattr(self.params, a) for a in paramsAttr}
         return paramsDict
 
-    def create_run(self):
+    def create_run(self, mode='async'):
         self.run = neptune.init_run(project=self.projectName, api_token=self.api,
                                     source_files=self.sourceFiles,
-                                    capture_hardware_metrics=True)
+                                    capture_hardware_metrics=True,
+                                    mode=mode)
         self.run['dataset'].track_files(self.dataDir)
-        if type(self.params) is Params:
-            self.run['parameters'] = self.transform_params_in_dict()
-        elif type(self.params) is dict:
-            self.run['parameters'] = self.params
-        else:
-            raise TypeError('params should be either Params or dict')
+        if self.params is not None:
+            if type(self.params) is Params:
+                self.run['parameters'] = self.transform_params_in_dict()
+            elif type(self.params) is dict:
+                self.run['parameters'] = self.params
+            else:
+                raise TypeError('Params should be either Params or dict')
 
     def log_parameters(self, paramName, paramValue, firstLevel = ''):
         self.run[f'{firstLevel}/{paramName}'] = paramValue
@@ -67,7 +64,7 @@ class NeptuneWrapper():
         from neptune.integrations.tensorflow_keras import NeptuneCallback
         return NeptuneCallback(run=self.run[level])
     
-    def get_pytorch_callback(self, model, level='training'):
+    def get_pytorch_callback(self, model):
         from neptune_pytorch import NeptuneLogger
         npt_logger = NeptuneLogger(run=self.run,
                                    model=model,
--- tests/neptune_wrapper_test.py
+++ tests/neptune_wrapper_test.py
@@ -1,11 +1,65 @@
-# Writing comprehensive unit tests for the NeptuneWrapper module
-
 import pytest
-from unittest.mock import MagicMock, patch
-from neptune_wrapper.neptune_wrapper import NeptuneWrapper
+from pathlib import Path
+import shutil
+from ..neptune_wrapper.neptune_wrapper import NeptuneWrapper
+from ..neptune_wrapper.utils import Params
 
 @pytest.fixture
-def neptune_client_mock():
-    # Mocking the Neptune client
-    neptune_mock = MagicMock()
-    return neptune_mock
+def parameters_dict():
+    return {
+        'param1': 1,
+        'param2': 2
+    }
+
+@pytest.fixture
+def parameters():
+    return Params(param1=1, param2=2)
+
+@pytest.fixture
+def neptune_wrapper_with_dict_params(parameters_dict):
+    wrapper = NeptuneWrapper(projectName='dummy_project', params=parameters_dict)
+    return wrapper
+
+@pytest.fixture
+def neptune_wrapper_params(parameters):
+    wrapper = NeptuneWrapper(projectName='dummy_project', params=parameters)
+    return wrapper
+
+def test_neptune_wrapper_logs_right_projects(neptune_wrapper_params):
+    assert neptune_wrapper_params.projectName == 'dummy_project'
+
+@pytest.mark.parametrize('nwrapper',
+                         ['neptune_wrapper_with_dict_params', 'neptune_wrapper_params'])
+def test_create_run_valid(nwrapper, request):
+    # Creating a Neptune run in offline mode to avoid sending requests to the Neptune server
+    wrapper = request.getfixturevalue(nwrapper)
+    wrapper.create_run(mode='offline')
+    id = wrapper.run._id
+    wrapper.stop_run() # to kill the process
+    assert Path(f'.neptune/offline/run__{id}').exists()
+
+def test_create_run_invalid(neptune_wrapper_params):
+    with pytest.raises(ValueError):
+        neptune_wrapper_params.create_run(mode='invalid')
+
+def test_tensorflow_callback_is_created(neptune_wrapper_params):
+    neptune_wrapper_params.create_run(mode='offline')
+    callback = neptune_wrapper_params.get_tensorflow_keras_callback()
+    neptune_wrapper_params.stop_run() # to kill the process
+    assert callback.__class__.__name__ == 'NeptuneCallback'
+
+def test_pytorch_callback_is_created(neptune_wrapper_params):
+    import torch
+    model = torch.nn.Sequential(torch.nn.Linear(1, 1))
+    neptune_wrapper_params.create_run(mode='offline')
+    callback = neptune_wrapper_params.get_pytorch_callback(model)
+    neptune_wrapper_params.stop_run() # to kill the process
+    assert callback.__class__.__name__ == 'NeptuneLogger'
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup(request):
+    """Cleanup a testing directory once we are finished."""
+    def remove_test_dir():
+        shutil.rmtree('.neptune', ignore_errors=True)
+    request.addfinalizer(remove_test_dir)
